831d2aea55cbb2fdcafc72e802abc519874c4291
Niuro Orlando Gabriel Langa <gabriel.niuro@gmail.com>
2026-04-27 01:46:58 +0200

    Api Configuration (Spring Boot)

---
+++ api/src/main/java/dev/niuro/sacm/SacmApplication.java
@@ -0,0 +1,13 @@
+package dev.niuro.sacm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacmApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/niuro/sacm/SacmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.niuro.sacm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
